2941a95d2d4487d6f7947be834a340ac125e99a4
mgonzalezjr7 <miguel.gonzalezjr7@gmail.com>
2023-05-23 06:00:09 -0700

    Added functionality to the save button.

--- Develop/script.js
+++ Develop/script.js
@@ -11,13 +11,16 @@ $(function () {
   
   $(".saveBtn").on("click", function () {
     var text = ${this}.siblings(".description").val();
-  })
+    var hour = $(this).parent().attr("id");
+    localStorage.setItem(hour, text);
+    JSON.paarse(localStorage.getItem(time, text));
+  });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
